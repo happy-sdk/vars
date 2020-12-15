cc218635e4f29749326c0de6e85a4a2d4524c605
github-action-benchmark <github@users.noreply.github.com>
2020-12-15 13:23:37 +0000

    add Benchmark (go) benchmark result for b3a639d5dea27c2fadb2dcb02754fd5bb6a5354b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608036630344,
+  "lastUpdate": 1608038617153,
   "repoUrl": "https://github.com/mkungla/vars",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 14.6,
             "unit": "ns/op",
             "extra": "84475435 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "marko.kungla@gmail.com",
+            "name": "Marko Kungla",
+            "username": "mkungla"
+          },
+          "committer": {
+            "email": "marko.kungla@gmail.com",
+            "name": "Marko Kungla",
+            "username": "mkungla"
+          },
+          "distinct": true,
+          "id": "b3a639d5dea27c2fadb2dcb02754fd5bb6a5354b",
+          "message": "use forked github-action-benchmark",
+          "timestamp": "2020-12-15T15:22:48+02:00",
+          "tree_id": "4c381aa941a99cdab3dee5d31664df3f324d6d2f",
+          "url": "https://github.com/mkungla/vars/commit/b3a639d5dea27c2fadb2dcb02754fd5bb6a5354b"
+        },
+        "date": 1608038615317,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkNew",
+            "value": 12.9,
+            "unit": "ns/op",
+            "extra": "90006469 times\n2 procs"
           }
         ]
       }
