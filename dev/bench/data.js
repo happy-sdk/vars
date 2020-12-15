cd3c466c1d01d6fe74fa1a30617c43632a37af71
github-action-benchmark <github@users.noreply.github.com>
2020-12-15 12:50:30 +0000

    add Benchmark (go) benchmark result for cf45c31820ce4668b9b75971b982d431580a90c4

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1608036630344,
+  "repoUrl": "https://github.com/mkungla/vars",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "marko.kungla@gmail.com",
+            "name": "Marko Kungla",
+            "username": "mkungla"
+          },
+          "committer": {
+            "email": "marko.kungla@gmail.com",
+            "name": "Marko Kungla",
+            "username": "mkungla"
+          },
+          "distinct": true,
+          "id": "cf45c31820ce4668b9b75971b982d431580a90c4",
+          "message": "run single benchmark to test bench reporting result regex",
+          "timestamp": "2020-12-15T14:49:36+02:00",
+          "tree_id": "325f3909392353d551ca828b13ca287903ceb2b5",
+          "url": "https://github.com/mkungla/vars/commit/cf45c31820ce4668b9b75971b982d431580a90c4"
+        },
+        "date": 1608036623918,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkNew",
+            "value": 14.6,
+            "unit": "ns/op",
+            "extra": "84475435 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
